e33e0a97664ae5e1c4826df3f756a4a89bd7c650
power10dan <lintzu@onid.oregonstate.edu>
2019-11-20 20:19:13 -0800

    Completed Spark Object Builder And XML Builder For Extracting Pubmed XML Files, Also Included Initial Work On Updating Schema With New Update Pubmed Data

--- code/dan_work/improved_ngd_comp/SparkLoadXMLBuilder.py
+++ code/dan_work/improved_ngd_comp/SparkLoadXMLBuilder.py
@@ -6,56 +6,73 @@ from config_file import bulk_path
 
 from SparkObjectBuilder import SparkObjBuilderInterface
 from SparkObjectBuilder import SparkObjConcreteConstructor
+from abc import ABCMeta, abstractmethod
 
-class SparkLoadXMLBuilder(metaclass=abc.ABCMeta):
+class SparkLoadXMLBuilder(metaclass=ABCMeta):
 	def __init__(self):
 		spark_constructor = SparkObjConcreteConstructor()
 		interface = SparkObjBuilderInterface()
-		# our dataframe
 		self.product = interface.construct(spark_constructor)
-	@abc.abstract_method
-	def _add_builder(self):
+	@abstractmethod
+	def _add_sample_update_data(self):
 		pass
-	@abc.abstract_method
-	def _add_appname(self):
+	@abstractmethod
+	def _add_sample_data(self):
 		pass
-	@abc.abstract_method
-	def _add_host_conf(self):
-		pass
-	@abc.abstract_method
-	def _add_port_conf(self):
+	@abstractmethod
+	def _add_entire_data(self):
 		pass
 
-class SparkLoadXMLConstructor(SparkObjectConstruct):
-	def _add_format(self):
-		self.product.format(data_brick)
-	def _add_options(self):
-		self.product.options(rowTag=row_tag)
-	def _add_load(self, schema_context):
-		self.product.load(bulk_path, schema=schema_context)
-	def _add_load_sample(self):
-		self.product.load(bulk_path)
-	def _add_sample(self):
-		self.product.load(load_xml_path).limit(10)
-	def gen_schema(self):
-		self.product.schema
+class SparkLoadXMLConstructor(SparkLoadXMLBuilder):
+	def _add_sample_update_data(self):
+		print("+++++++ SAMPLING DAILY UPDATE SCHEMA ++++++++")
+		df = self.product.read\
+						 .format(data_brick)\
+						 .load(sample_xml_path).limit(10)
+		print("+++++++ FINISHED SAMPLING DAILY UPDATE SCHEMA ++++++++")
+		return df
+	def _add_update_data(self, pre_comp_schema):
+		print("+++++++ LOADING ENTIRE UPDATE XML SCHEMA +++++++")
+		entire_df = self.product.read\
+								.format(data_brick)\
+								.load(bulk_sample_path, schema=pre_comp_schema)
+		print("+++++++ FINISHED LOADING ENTIRE UPDATE XML SCHEMA+++++++")
+		return entire_df
+
+	def _add_sample_data(self):
+		print("++++ SAMPLING XML FILE TO GEN SCHEMA +++++")
+		df = self.product.read\
+					.format(data_brick)\
+					.options(rowTag=row_tag)\
+					.load(load_xml_path).limit(10)
+		print("++++++++ FINISHED SAMPLING ++++++++++++")
+		return df
+	def _add_entire_data(self, df, pre_comp_schema):
+		print("++++ LOADING ENTIRE XML FILES IN FOLDER +++++")
+		entire_df = self.product.read\
+								.format(data_brick)\
+					  			.options(rowTag=row_tag)\
+					  			.load(bulk_path, schema=pre_comp_schema)
+		print("++++ FINISHED ENTIRE XML FILES IN FOLDER +++++")
+		return entire_df
 	def get_product(self):
 		return self.product
 
 class SparkLoadXMLInterface:
 	def __init__(self):
 		self.builder = None
-	def construct_reg(self, conc_builder, schema):
-		self.builder=conc_builder
-		self.builder._add_format()
-		self.builder._add_options()
-		self.builder._add_load(schema)
-		return conc_builder.get_product()
+	def construct_reg(self, conc_builder, schema_ctx, df):
+		self.builder = conc_builder
+		return self.builder._add_entire_data(df, schema_ctx)
+
 	def construct_sample(self, conc_builder):
 		self.builder=conc_builder
-		self.builder._add_format()
-		self.builder._add_options()
-		self.builder._add_load_sample()
-		self.builder._add_sample()
-		self.builder.gen_schema()
-		return conc_builder.get_product()
+		return self.builder._add_sample_data()
+
+	def construct_sample_update(self, conc_builder):
+		self.builder = conc_builder
+		return self.builder._add_sample_update_data()
+
+	def construct_entire_update(self, conc_builder, schema_ctx)
+		return conc_builder._add_update_data(schema_ctx)
+
--- code/dan_work/improved_ngd_comp/SparkObjectBuilder.py
+++ code/dan_work/improved_ngd_comp/SparkObjectBuilder.py
@@ -1,45 +1,57 @@
+import sys
+sys.path.append('.')
+
 from pyspark.sql import SparkSession
+from pyspark.conf import SparkConf
 from config_file import app_name
 from config_file import host_name
 from config_file import port_name
-from config_file import spark_redis_host
-from config_file import spark_redis_port
+from config_file import spark_mem_config
+from config_file import spark_redis_jar
+from abc import ABCMeta, abstractmethod
 
-class SparkObjectConstruct(metaclass=abc.ABCMeta):
+class SparkObjectConstruct(metaclass=ABCMeta):
 	def __init__(self):
-		self.product = SparkSession()
-	@abc.abstract_method
-	def _add_builder(self):
-		pass
-	@abc.abstract_method
-	def _add_appname(self):
-		pass
-	@abc.abstract_method
-	def _add_host_conf(self):
-		pass
-	@abc.abstract_method
-	def _add_port_conf(self):
+		self.product = SparkConf()
+	#@abstractmethod
+	#def _add_host_conf(self):
+	#	pass
+	#@abstractmethod
+	#def _add_port_conf(self):
+	#	pass
+	@abstractmethod
+	def _add_conf_memory(self):
 		pass
 
 class SparkObjConcreteConstructor(SparkObjectConstruct):
-	def _add_builder(self):
-		self.product.builder
-	def _add_appname(self):
-		self.product.appName(app_name)
-	def _add_host_conf(self):
-		self.product.config(spark_redis_host, host_name)
-	def _add_port_conf(self):
-		self.product.config(spark_redis_port, port_name)
+	#def _add_host_conf(self):
+	#	self.product.set(spark_redis_host, host_name)
+	#def _add_port_conf(self):
+	#	self.product.set(spark_redis_port, port_name)
+	def _add_conf_memory(self):
+		self.product.setAll(spark_mem_config)
+	def _add_jars_conf(self):
+		# add jar dependencies.
+		self.product.set('spark.jars.packages',\
+						 'com.databricks:spark-xml_2.11:0.6.0')
+		#self.product.set('spark.jars', spark_redis_jar)
 	def get_product(self):
 		return self.product
 
 class SparkObjBuilderInterface:
 	def __init__(self):
 		self.builder = None
+
 	def construct(self, conc_builder):
 		self.builder=conc_builder
-		self.builder._add_builder()
-		self.builder._add_appname()
-		self.builder._add_host_conf()
-		self.builder._add_port_conf()
-		return conc_builder.get_product()
+		self.builder._add_conf_memory()
+		#self.builder._add_host_conf()
+		#self.builder._add_port_conf()
+		self.builder._add_jars_conf()
+		spark_conf = self.builder.get_product()
+		print("+++++++GENERATING SPARK CONTEXT ++++++++++")
+		spark = SparkSession.builder\
+						   .config(conf = spark_conf)\
+						   .getOrCreate()
+		print("+++++++FINISHED GENERATING SPARK CONTEXT ++++++++++")
+		return spark
