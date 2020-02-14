76048ab39ebe816a5ab793a40e51970500243c78
power10dan <lintzu@onid.oregonstate.edu>
2020-02-13 23:13:53 -0500

    Comments Added On What Each Variable Mean

--- code/reasoningtool/QuestionAnswering/config_file.py
+++ code/reasoningtool/QuestionAnswering/config_file.py
@@ -1,21 +1,30 @@
 app_name = "NGD_APP"
+# host of where you placed
+# your pyspark server. If running
+#locally, simply use local host
+# and current port name.
 host_name = "localhost"
 xml_path = ""
 port_name = "6379"
 data_brick = 'com.databricks.spark.xml'
+# tag for parsing the xml
 row_tag = "MedlineCitation"
 update_tag = "DeleteCitation"
 sample_xml_path = "/Users/daniellin/Desktop/PubMed/update/sample_update.xml"
 bulk_sample_path = "/Users/daniellin/Desktop/PubMed/update/"
-# location for your sample xml for spark to load
+# location for your sample xml for spark to load.
+# you should just take one file from your bulk path,
+# copy and paste them into the load_xml_path,
+# and just leave it there. The purpose of this
+# file is to let spark quickly build up
+# the schema to load XML in bulks quickly.
 load_xml_path = "/Users/daniellin/Desktop/sample/sample_for_loading2.xml"
-# where your bulk XML files are
+# where your bulk XML files that you downloaded from pub med
+# should reside in
 bulk_path = "/Users/daniellin/Desktop/PubMed/"
-# spark general config
+# spark general configuration.
 spark_mem_config = [('spark.executor.memory', '3g'), \
 					('spark.app.name', 'XMLExtractor'), \
 					('spark.executor.cores', '2'), \
 					('spark.cores.max', '4'), \
 					('spark.driver.memory','4g')]
-# spark-redis connector jar path
-spark_redis_jar = '/Users/daniellin/Desktop/OSU_Job/RTX/code/dan_work/spark-redis/target/spark-redis-2.4.1-SNAPSHOT-jar-with-dependencies.jar'
